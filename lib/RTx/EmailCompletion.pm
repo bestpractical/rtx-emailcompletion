b6868819b9a77e52959eb7c2f9319a478eb0edb7
Nicolas Chuche <nicolas@barna.be>
2007-04-09 00:55:55 +0000

    1st import

---
+++ lib/RTx/EmailCompletion.pm
@@ -0,0 +1,48 @@
+package RTx::EmailCompletion;
+
+use strict;
+
+our $VERSION = "0.01";
+
+1;
+
+__END__
+
+=head1 NAME
+
+RTx::EmailCompletion - Auto completion for email field
+
+=head1 VERSION
+
+This document describes version 0.01 of RTx::EmailCompletion
+
+=head1 DESCRIPTION
+
+Add ajax autocompletion on email field
+
+=head1 INSTALLATION
+
+Install it like a standard perl module
+
+ perl Makefile.PL
+ make
+ make install
+
+
+=head1 AUTHORS
+
+Nicolas Chuche E<lt>nchuche@barna.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2007 by Nicolas Chuche E<lt>nchuche@barna.beE<gt>
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+All Scriptaculous and Prototype program are placed under MIT licence
+and are copyrighted by their owner (see top of files).
+
+=cut
