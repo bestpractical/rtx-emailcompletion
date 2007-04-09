023e5395c25c0f78bdfda3ddce2b9165b06eb7ea
Nicolas Chuche <nicolas@barna.be>
2007-04-09 14:51:53 +0000

    change name of Regexp and the name of the webservice. Delete old no longer used function. Add comment

--- html/NoAuth/js/emailcompletion.js
+++ html/NoAuth/js/emailcompletion.js
@@ -2,24 +2,21 @@ function initPage() {
     addAutoComplete();
 } // initPage
 
+/* CUSTOMIZE PART */
+
+// documentation can be found in lib/RTx/EmailCompletion.pm
+
 // we need two Arrays :
 // one for input with multiple email address allowed
 var multipleCompletion = new Array("Requestors", "Cc", "AdminCc", "WatcherAddressEmail[123]", "UpdateCc", "UpdateBcc");
 
 // and one for input with only one email address allowed
-var uniqueCompletion   = new Array("(Add|Delete)Requestor", "(Add|Delete)Cc", "(Add|Delete)AdminCc");
+var singleCompletion   = new Array("(Add|Delete)Requestor", "(Add|Delete)Cc", "(Add|Delete)AdminCc");
 
-var Regexp         = new RegExp('^(' + multipleCompletion.concat(uniqueCompletion).join('|') + ')$');
+/* END OF CUSTOMIZE PART */
+
+var globalRegexp   = new RegExp('^(' + multipleCompletion.concat(singleCompletion).join('|') + ')$');
 var multipleRegexp = new RegExp('^(' + multipleCompletion.join('|') + ')$');
-
-function findRegexpInArray(string) {
-    for (var i = 0; i < InputNames.length; i++) {
-	regexp = new RegExp('^' + InputNames[i]+'$');
-	if (string.match(regexp)) {
-	    return 1;
-	}
-    }
-}
 
 function addAutoComplete() {
     var inputs = document.getElementsByTagName("input");
@@ -28,16 +25,17 @@ function addAutoComplete() {
 	var input = inputs[i];
 	var inputName = input.getAttribute("name");
 
+	// if empty or not defined
 	if (! inputName)
 	    continue;
 
 	// only input's names defined in global vars at the beginning
 	// are concerned
-	if ( ! inputName.match(Regexp) )
+	if ( ! inputName.match(globalRegexp) )
 	    continue;
 
-	// if multiple email address allowed we add an tokens option
-	// to Autocompleter
+	// if multiple email address are allowed we add an tokens
+	// option to Autocompleter
 	var options = '';
 	if (inputName.match(multipleRegexp))
 	    options = "tokens: ','"
@@ -47,7 +45,7 @@ function addAutoComplete() {
 	// FIXME "je suis la"
 	var div = '<div class="autocomplete" id="' + inputName + '_to_auto_complete">je suis la</div>';
 	div += '<script type="text/javascript">new Ajax.Autocompleter(\'' + inputName;
-	div += "', '" + inputName + "_to_auto_complete', '<%$RT::WebPath%>/Ajax/Email\', {" + options + "})</script>";
+	div += "', '" + inputName + "_to_auto_complete', '<%$RT::WebPath%>/Ajax/EmailCompletion\', {" + options + "})</script>";
 
 	// use prototype to add the div after input
 	new Insertion.After(inputName,div);
