b8c104c899735eec667a572784c8b37f65be4477
Nicolas Chuche <nicolas@barna.be>
2007-04-09 01:06:39 +0000

    add doc and typo

--- lib/RTx/EmailCompletion.pm
+++ lib/RTx/EmailCompletion.pm
@@ -18,7 +18,31 @@ This document describes version 0.01 of RTx::EmailCompletion
 
 =head1 DESCRIPTION
 
-Add ajax autocompletion on email field
+I'm so tired to type email address by hand that I've done this module
+try Add ajax autocompletion on email field
+
+There's 3 S<things :>
+
+=over
+
+=item *
+
+a small web service C<html/Ajax/Email> which search in all known users
+in the database
+
+=item *
+
+the scriptaculous library to autocomplete
+
+=item *
+
+and modified html RT pages to use those two first component
+
+=back
+
+As I've embedded some pages of RT, it should work only for release
+3.6.3. But I keep the patches of all modified files in C<patches/> so
+you can try to use them against other release.
 
 =head1 INSTALLATION
 
@@ -43,6 +67,6 @@ modify it under the same terms as Perl itself.
 See L<http://www.perl.com/perl/misc/Artistic.html>
 
 All Scriptaculous and Prototype program are placed under MIT licence
-and are copyrighted by their owner (see top of files).
+and are copyrighted by their owners (see top of files).
 
 =cut
